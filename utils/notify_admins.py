30d60ffbfb092b49a1b52f553f517186c42307de
rakhimberdiyev <163638265+rakhimberdiyev@users.noreply.github.com>
2024-04-03 13:26:20 +0500

    Update notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -4,19 +4,24 @@ import logging
 from aiogram import Bot, Dispatcher
 from data.config import ADMINS
 
-
-async def on_startup_notify(dp: Dispatcher, user: str):
+message_ids = {}
+async def on_startup_notify(dp: Dispatcher, user: str, id):
     for admin in ADMINS:
         try:
             if user != None:
-                if 'Filial' not in user:
-                    await dp.bot.send_message(chat_id="-1002102115794",
-                                        text=f"{user}", parse_mode=None)
-                    
-                elif 'Filial' in user:
-                    msg = await dp.bot.send_message(chat_id="-1002102115794",
-                                        text=f"{user}\n\nUshbu user barcha jarayonni yakunladi", parse_mode=None)
-                    await dp.bot.delete_message(chat_id='-1002102115794', message_id=msg.message_id-1)
+                    if 'Filial' not in user:
+                        msg = await dp.bot.send_message(chat_id="-1002102115794",
+                                                        text=f"{user}", parse_mode=None)
+                        message_ids[id] = msg.message_id
+
+                    elif 'Filial' in user:
+                        await dp.bot.send_message(chat_id="-1002102115794",
+                                                  text=f"{user}\n\nUshbu user barcha jarayonni yakunladi")
+                        last_msg_id = message_ids[id]
+                        await dp.bot.delete_message(chat_id='-1002102115794',
+                                                    message_id=last_msg_id)
+
+                        del message_ids[id]
             else:
                 await dp.bot.send_message(admin, "Bot ishga tushdi")
 
